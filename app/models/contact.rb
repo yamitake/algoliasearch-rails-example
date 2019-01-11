fc7fc34a7d020dd0d014629734af181e8f5c1d17
yamitake <take.yapr@gmail.com>
2019-01-11 19:17:57 +0900

    temp

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,8 @@ class Contact < ActiveRecord::Base
 
   include AlgoliaSearch
   algoliasearch per_environment: true do
-    attributesToIndex ['name', 'unordered(email)', 'company']
-    attributesForFaceting ['company']
+    attributesToIndex ['name', 'unordered(email)', 'current_company_name']
+    attributesForFaceting ['current_company_name']
   end
 
 end
